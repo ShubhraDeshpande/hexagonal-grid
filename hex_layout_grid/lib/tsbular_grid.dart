ed4d27f3f90f43a01655c7fa593fece64b40f8e7
Shubhra <shubhraj@buffalo.edu>
2019-12-11 15:42:31 -0500

    minor

--- hex_layout_grid/lib/tsbular_grid.dart
+++ hex_layout_grid/lib/tsbular_grid.dart
@@ -15,7 +15,7 @@ class HexGridWidgetExample extends StatelessWidget {
   final double _scaleFactor = 0.2;
   final double _densityFactor = 1.75;
   final double _velocityFactor = 0.0;
-  final int _numOfHexGridChildWidgets = 8;
+  final int _numOfHexGridChildWidgets = 36;
 
   @override
   Widget build(BuildContext context) {
@@ -90,7 +90,7 @@ return ClipPolygon(
   PolygonBoxShadow(color: Colors.black, elevation: 1.0),
   PolygonBoxShadow(color: Colors.grey, elevation: 5.0)
  ],
- child: Container(color: Colors.limeAccent),
+ child: Container(color: Colors.orangeAccent),
 );
 
   }
